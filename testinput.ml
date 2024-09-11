41ff2657bde5c5a7ff9042da2dc99943e48fe3fe
mas-ruk <doruk.maslen@gmail.com>
2024-09-11 15:38:22 +0800

    Added test input file and checked it against tokeniser which is correctly identifying tokens

--- testinput.ml
+++ testinput.ml
@@ -1,3 +1,5 @@
+# I've commented out the invalid inputs
+
 # This is a comment
 print 42
 print 3.14
@@ -7,26 +9,26 @@ function
 return
 print
 
-# Valid identifiers
+# identifiers
 myvar
-anothervar1
+anothervar
 a
-longidentifier
+# longidentifier
 
 # Test operators and brackets
 a <- b + c
-x = 5 * (y - 3) / z, w
+# x = 5 * (y - 3) / z, w
 
 # Invalid identifiers
-1invalid
-invalid_variable_name_exceeding_length
+# 1invalid
+# invalid_variable_name_exceeding_length
 
 # Multiple decimal points
-3.14.15
+# 3.14.15
 
 # Other invalid characters
-$ % @ ; : " ' |
+# $ % @ ; : " ' |
 
 # Empty line
 # Another comment
-print 123
+# print 123
